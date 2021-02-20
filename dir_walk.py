c1d908ff6ee15cb2a31d872d61886e71525a237b
Stephen Hulme <shulme801@gmail.com>
2021-02-20 14:55:52 -0500

    20 Feb Got regular expression working

--- dir_walk.py
+++ dir_walk.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
 import os
+import re
 
 def dir_walk(dir):
     cwd = os.getcwd()
 
-    #we shall store all the file names in this list
+    #we will store all the file names in this list
     file_list = []
 
     for root, dirs, files in os.walk(cwd):
@@ -14,5 +15,33 @@ def dir_walk(dir):
             file_list.append(os.path.join(root,file))
     return(file_list)
 #print all the file names
-for name in dir_walk("/Volumes/Hulme_Local_Extended/src/Python/DirScan/test_data"):
-    print(name)
+
+def test_unpacking(**kwargs):
+    #kwargs is a dict
+
+    ignore   = kwargs.get("ignore")
+    search   = kwargs.get("search")
+
+    print(ignore)
+    ignotus = ignore.search('Volumes/Hulme_Local_Extended/src/Python/DirScan/.git/packed-refs')
+    if ignotus:
+        print('match found: ',ignotus.group())
+    else:
+        print('match not found')
+
+    
+    
+# for name in dir_walk("/Volumes/Hulme_Local_Extended/src/Python/DirScan/test_data"):
+#     print(name)
+hidden_files     = re.compile(r'/\.\w+')
+searchable_files = re.compile(r'\.txt$')
+test_unpacking(ignore=hidden_files,search=searchable_files)
+# x = re.compile(hidden_files)
+# m = x.search('/Volumes/Hulme_Local_Extended/src/Python/DirScan/.git/packed-refs')
+# if m:
+#         print('match found: ',m.group())
+# else:
+#         print('match not found')
+
+
+# 
