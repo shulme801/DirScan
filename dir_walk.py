a4aa4c1e7b6f9d139a67f8401129c6120e0017e1
Stephen Hulme <shulme801@gmail.com>
2021-02-20 13:17:08 -0500

    20 Feb Add test data

--- dir_walk.py
+++ dir_walk.py
@@ -2,16 +2,17 @@
 
 import os
 
-cwd = os.getcwd()
+def dir_walk(dir):
+    cwd = os.getcwd()
 
-#we shall store all the file names in this list
-filelist = []
+    #we shall store all the file names in this list
+    file_list = []
 
-for root, dirs, files in os.walk(cwd):
-	for file in files:
-        #append the file name to the list
-		filelist.append(os.path.join(root,file))
-
+    for root, dirs, files in os.walk(cwd):
+        for file in files:
+            #append the file name to the list
+            file_list.append(os.path.join(root,file))
+    return(file_list)
 #print all the file names
-for name in filelist:
+for name in dir_walk("/Volumes/Hulme_Local_Extended/src/Python/DirScan/test_data"):
     print(name)
