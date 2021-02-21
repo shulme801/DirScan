862aeb4740b5501c723d041932cb47a5d79a0fa8
Stephen Hulme <shulme801@gmail.com>
2021-02-21 17:20:40 -0500

    21 Feb last update

--- test_driver.py
+++ test_driver.py
@@ -13,7 +13,7 @@ searchable_files = ""
 hidden_dirs      = re.compile(r'\.\w*')
 searchable_files = re.compile(r'\.txt$')
 top_dir          = '/Volumes/Hulme_Local_Extended/src/Python/DirScan/test_data'
-print('------------------------------')
+print('------------------------------') # Strictly for show
 file_list = dir_walk(root=top_dir, ignore=hidden_dirs, search=searchable_files)
 word_list = ["there", "Michael", "running", "man"]
 print("Final word counts are {0}".format(count_all_occurrences(word_list, file_list)))
